a16155a8b2b714c362e794d87736e2de03068e61
Phil-G-M21 <jyaw456@gmail.com>
2026-06-27 07:09:23 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // PASTE YOUR GOOGLE APPS SCRIPT WEB APP URL BELOW
 // It looks like: https://script.google.com/macros/s/XXXXXXX/exec
 
-window.SHEET_URL = "PASTE_YOUR_URL_HERE";
+window.SHEET_URL = "https://script.google.com/macros/s/AKfycbxlcaj0X_Hkp2Z8bFnV7eA__OCubxaChQQHCd3LmctecTl-PmsWoZI9CCZOyrq7Agip/exec";
